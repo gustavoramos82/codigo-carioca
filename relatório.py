c70fa92987df344afe7e5f3e7bbfb63e8b01e4b0
Gustavo Ramos <39843884+gustavoramos82@users.noreply.github.com>
2024-04-18 20:31:24 -0300

    Update relatório.py

--- relatório.py
+++ relatório.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 
 # Importando o datasets
-barragem = pd.read_csv('/home/gustavo/Documentos/teste/dataframe final.csv',encoding='latin1')
+barragem = pd.read_csv('https://raw.githubusercontent.com/gustavoramos82/codigo-carioca/main/Dataset/dataframe%20usado%20no%20streamlit.csv',encoding='latin1')
 
 topicos = ['Sobre o projeto','Análise inicial','Análise por mapa','Análise por cluster']
 barra_lateral = st.sidebar.empty()
